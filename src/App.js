081869820b750efaec67ac2aeab7254a11c8e94f
Alexandr <semakaleksandr2014@gmail.com>
2018-06-27 18:11:45 +0300

    list on screen ready

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react';
+import Note from './Note/Note'
+import NoteForm from './NoteForm/NoteForm'
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      notes: [
+        {id: 1, noteContent: 'Note 1 !'},
+        {id: 2, noteContent: 'Note 2 !'}
+      ],
+    }
+  }
+
   render() {
     return (
       <div className="App">
-        App.js
+        <h1>ToDo List App</h1>
+          {
+            this.state.notes.map(note =>
+              <Note noteContent={note.noteContent} noteId={note.id} key={note.id} />
+            )
+          }
+          
+        <NoteForm/>
       </div>
     );
   }
